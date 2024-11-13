f9d68bac7308ffe8c0521f560323d54d3d617df1
Mariana Marinho <116121780+Marimarinho@users.noreply.github.com>
2024-11-13 17:57:33 -0300

    Delete jss directory

--- jss/script1.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const navbarContainer = document.querySelector('.navbar-container');
-
-menuIcon.addEventListener('click', () => {
-    navbarContainer.classList.toggle('open');
-});
